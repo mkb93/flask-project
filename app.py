f4f03c6025864bf98e894b1e0cc53657225687e8
Marco Karim belahouane <nfk18@hotmail.co.uk>
2025-03-13 13:50:05 +0000

    managed to connect to supabase

--- app.py
+++ app.py
@@ -1,24 +1,74 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+import requests
 import psycopg2
 import os
+from dotenv import load_dotenv
+from flask_cors import CORS
 
+
+load_dotenv()
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 app = Flask(__name__)
+CORS(app)
 
+print(SUPABASE_URL, SUPABASE_KEY)
 # Database connection
-DATABASE_URL = os.getenv("https://byfmhdwlnatdmivajvqq.supabase.co", "postgresql://postgres:w.96aRFcy6Gs6RQ@db.byfmhdwlnatdmivajvqq.supabase.co:5432/postgres")
+# DATABASE_URL = os.getenv("https://byfmhdwlnatdmivajvqq.supabase.co", "postgresql://postgres:w.96aRFcy6Gs6RQ@db.byfmhdwlnatdmivajvqq.supabase.co:5432/postgres")
 
-def get_db_connection():
-  conn = psycopg2.connect(DATABASE_URL)
-  return conn
+# def get_db_connection():
+#   conn = psycopg2.connect(DATABASE_URL)
+#   return conn
 
-@app.route("/")
-def home():
-  conn = get_db_connection()
-  cursor = conn.cursor()
-  cursor.execute("SELECT 'Hello from the database!'")
-  result = cursor.fetchone()
-  conn.close()
-  return jsonify({"message": result[0]})
+# test
+@app.route('/test-db', methods=['GET'])
+def test_db():
+  headers = {
+    "apikey": SUPABASE_KEY,
+    "Authorization": f"Bearer {SUPABASE_KEY}"
+  }
+  response= requests.get(f"{SUPABASE_URL}/rest/v1/items", headers=headers)
+
+  if response.status_code != 200:
+    return jsonify({"error": response.json()}), response.status_code
+  
+  return jsonify(response.json())
+
+@app.route('/add-item', methods=['POST'])
+def add_item():
+  data = request.json
+  headers = {
+    "apikey": SUPABASE_KEY,
+    "Authorization": f"Bearer {SUPABASE_KEY}",
+     "Content-Type": "application/json"
+    }
+
+  payload = {
+    "name": data.get("name"),
+    "location": data.get("location")
+    }
+
+  response = requests.post(f"{SUPABASE_URL}/rest/v1/items", json=payload, headers=headers)
+
+  if response.status_code != 201:
+    return jsonify({"error": response.json()}), response.status_code
+
+  return jsonify(response.json())
+# @app.route("/")
+# def home():
+#   conn = get_db_connection()
+#   cursor = conn.cursor()
+#   cursor.execute("SELECT name FROM items")
+#   items = cursor.fetchall()
+
+#   items_list = [ 
+#     {"item": item[0]}
+#     for item in items
+#   ]
+#   cursor.close()
+#   conn.close()
+
+#   return jsonify(items_list)
 
 if __name__ == "__main__":
   app.run(debug=True)
