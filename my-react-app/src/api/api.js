fc5b17654a898e4ad9d4d767a1f54f017826974d
Marco Karim belahouane <nfk18@hotmail.co.uk>
2025-03-15 17:32:30 +0000

    corrected bug test-db

--- my-react-app/src/api/api.js
+++ my-react-app/src/api/api.js
@@ -5,7 +5,7 @@ const API_BASE_URL = 'https://flask-project-68m0.onrender.com';
 // Fetch items from the database
 export const getItems = async () => {
     try {
-        const response = await axios.get(`${API_BASE_URL}/test-db`);
+        const response = await axios.get(`${API_BASE_URL}/`);
         return response.data;
     } catch (error) {
         console.error("Error fetching items:", error);
