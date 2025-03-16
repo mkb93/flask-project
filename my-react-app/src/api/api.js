e77ce07e5d67d212893a6234238c62fad0e65a0f
Marco Karim belahouane <nfk18@hotmail.co.uk>
2025-03-16 22:41:03 +0000

    tidying and testing

--- my-react-app/src/api/api.js
+++ my-react-app/src/api/api.js
@@ -1,25 +1,22 @@
 import axios from 'axios';
 
 const API_BASE_URL = 'https://flask-project-68m0.onrender.com'; 
-
-// Fetch items from the database
 export const getItems = async () => {
-    try {
-        const response = await axios.get(`${API_BASE_URL}/`);
-        return response.data;
-    } catch (error) {
-        console.error("Error fetching items:", error);
-        return [];
-    }
+  try {
+      const response = await axios.get(`${API_BASE_URL}/`);
+      return response.data;
+  } catch (error) {
+      console.error("Error fetching items:", error);
+      return [];
+  }
 };
 
-// Add a new item
 export const addItem = async (item) => {
-    try {
-        const response = await axios.post(`${API_BASE_URL}/add-item`, item);
-        return response.data;
-    } catch (error) {
-        console.error("Error adding item:", error);
-        throw error;
-    }
+  try {
+      const response = await axios.post(`${API_BASE_URL}/add-item`, item);
+      return response.data;
+  } catch (error) {
+      console.error("Error adding item:", error);
+      throw error;
+  }
 };
