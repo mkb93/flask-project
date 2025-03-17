382b4d14282f3c4351d63a39c93f61ea7b40e7b4
Marco Karim belahouane <nfk18@hotmail.co.uk>
2025-03-17 11:15:17 +0000

    tidying app.py comments

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, jsonify, request
 import requests
-import psycopg2
 import os
 from dotenv import load_dotenv
 from flask_cors import CORS
@@ -12,15 +11,6 @@ SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 app = Flask(__name__)
 CORS(app)
 
-print(SUPABASE_URL, SUPABASE_KEY)
-# Database connection
-# DATABASE_URL = os.getenv("https://byfmhdwlnatdmivajvqq.supabase.co", "postgresql://postgres:w.96aRFcy6Gs6RQ@db.byfmhdwlnatdmivajvqq.supabase.co:5432/postgres")
-
-# def get_db_connection():
-#   conn = psycopg2.connect(DATABASE_URL)
-#   return conn
-
-# test
 @app.route('/', methods=['GET'])
 def home():
   headers = {
@@ -40,17 +30,6 @@ def add_item():
 
   print("Received data:", data)
 
-  # headers = {
-  #   "apikey": SUPABASE_KEY,
-  #   "Authorization": f"Bearer {SUPABASE_KEY}",
-  #    "Content-Type": "application/json"
-  #   }
-
-  # payload = {
-  #   "name": data.get("name"),
-  #   "location": data.get("location")
-  #   }
-
   response = requests.post(f"{SUPABASE_URL}/rest/v1/items/data", json=data)
   print("Response status:", response.status_code)
   print("Response text:", response.text)
@@ -60,21 +39,6 @@ def add_item():
     return jsonify(response.json())
   except requests.exceptions.JSONDecodeError:
     return jsonify({"error": "Invalid JSON response from Supabase"}), 500
-# @app.route("/")
-# def home():
-#   conn = get_db_connection()
-#   cursor = conn.cursor()
-#   cursor.execute("SELECT name FROM items")
-#   items = cursor.fetchall()
-
-#   items_list = [ 
-#     {"item": item[0]}
-#     for item in items
-#   ]
-#   cursor.close()
-#   conn.close()
-
-#   return jsonify(items_list)
 
 if __name__ == "__main__":
   app.run(debug=True)
