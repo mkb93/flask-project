5cb4f3c3bbcef85125a138709e236595c554cad3
Marco Karim belahouane <nfk18@hotmail.co.uk>
2025-03-24 01:04:37 +0000

    removed supabase depencies2

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ load_dotenv()
 url = os.getenv("SUPABASE_URL")
 key = os.getenv("SUPABASE_KEY")
 
-supabase = create_client(url, "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ5Zm1oZHdsbmF0ZG1pdmFqdnFxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg3MDE3NzEsImV4cCI6MjA1NDI3Nzc3MX0.ok9GxAzBL3vTDqv6K7uX4-zd0OjQ2tR1bcTGGM5fV94")
+# supabase = create_client(url, "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ5Zm1oZHdsbmF0ZG1pdmFqdnFxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg3MDE3NzEsImV4cCI6MjA1NDI3Nzc3MX0.ok9GxAzBL3vTDqv6K7uX4-zd0OjQ2tR1bcTGGM5fV94")
 
 data = supabase.table('items').insert({"name": "face7", 'location': 'face7'}).execute()
 data = supabase.table('items').insert({"name": "face6", 'location': 'face6'}).execute()
