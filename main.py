3eaca60f7fb1354a76978697f0be89d300c9838c
Marco Karim belahouane <nfk18@hotmail.co.uk>
2025-03-24 01:05:27 +0000

    removed supabase depencies3

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ key = os.getenv("SUPABASE_KEY")
 
 # supabase = create_client(url, "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ5Zm1oZHdsbmF0ZG1pdmFqdnFxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg3MDE3NzEsImV4cCI6MjA1NDI3Nzc3MX0.ok9GxAzBL3vTDqv6K7uX4-zd0OjQ2tR1bcTGGM5fV94")
 
-data = supabase.table('items').insert({"name": "face7", 'location': 'face7'}).execute()
-data = supabase.table('items').insert({"name": "face6", 'location': 'face6'}).execute()
-data = supabase.table('items').update({"name": "face8"}).eq("name", "face7").execute()
-data= supabase.table("items").delete().eq("name", "face6").execute()
+# data = supabase.table('items').insert({"name": "face7", 'location': 'face7'}).execute()
+# data = supabase.table('items').insert({"name": "face6", 'location': 'face6'}).execute()
+# data = supabase.table('items').update({"name": "face8"}).eq("name", "face7").execute()
+# data= supabase.table("items").delete().eq("name", "face6").execute()
 
-data = supabase.table("items").select("*").execute()
-print(data)
+# data = supabase.table("items").select("*").execute()
+# print(data)
